afa023f07fbe6fb9af3aea613bea3c67ca65027b
derrick <derrick.2a4@gmail.com>
2017-10-20 13:18:34 +0800

    Revert "Change accessibility of handler"
    This reverts commit 1154ecb61b61bab4a788e417071d0f9064dcb8f3.

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -25,6 +25,7 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.UserPrefs;
+
 import seedu.address.model.google.OAuth;
 import seedu.address.model.util.SampleDataUtil;
 import seedu.address.storage.AddressBookStorage;
